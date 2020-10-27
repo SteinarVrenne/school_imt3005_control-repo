8e5d6f8785bb7eb32c0cd534262987fb60c2cae5
Steinar Vrenne <steinavr@stud.ntnu.no>
2020-10-27 20:45:09 +0100

    Fixed slight typo in webserver/server.pp

--- site-modules/profile/manifests/webserver/server.pp
+++ site-modules/profile/manifests/webserver/server.pp
@@ -2,7 +2,7 @@ class profile::webserver::server {
     vcsrepo { '/home/ubuntu ':
         ensure => present,
         providet => git,
-        source => 'https://bitbucket.org/Jimbob21148/control-repo.git'
+        source => 'https://bitbucket.org/Jimbob21148/control-repo.git',
         revision => 'testing'
     }
 }
