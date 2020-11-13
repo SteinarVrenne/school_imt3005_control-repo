a5f45f9cbe1045ce517dbad72d215ab9580c8690
Harald <haraar@stud.ntnu.no>
2020-11-13 18:05:21 +0100

    Moved Kali to control repo

---
+++ scripts/kali/buildContainer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#docker images > temp.txt
+#if [[ ( $(grep -E 'pentest|forensics|stego' temp.txt)) ]]; then
+#        echo hei
+#else
+#    echo "Not installed"
+    docker pull haraldaarz/kali_xfce_vnc_top10
+
+#fi
