a16d6700d048e83776e37da3926ac9bcdb43a925
Steinar Vrenne <steinavr@stud.ntnu.no>
2020-11-16 15:08:35 +0100

    Changed keychecking to prevent script from failing from no user input in add_authorized_key.bash

--- add_authorized_key.bash
+++ add_authorized_key.bash
@@ -8,7 +8,7 @@ source /root/openRC.sh
 webip=$(openstack server list | grep web | awk '{print $9}')
 
 # SSH to webserver and generate key
-ssh root@$webip "ssh-keygen -oStrictHostKeyChecking=no -q -N '' -f /root/.ssh/id_rsa"
+ssh -oStrictHostKeyChecking=no root@$webip "ssh-keygen -q -N '' -f /root/.ssh/id_rsa"
 
 # cat the file contents and add to ubuntu's authorized key on manager
 ssh root@$webip "cat /root/.ssh/id_rsa.pub" >> /home/ubuntu/.ssh/authorized_keys
