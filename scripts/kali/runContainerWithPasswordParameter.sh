84d7234013d0fe34ebddefbd888565c8f5a22037
Harald <haraar@stud.ntnu.no>
2020-11-16 19:00:03 +0100

    Same message as commit below

--- scripts/kali/runContainerWithPasswordParameter.sh
+++ scripts/kali/runContainerWithPasswordParameter.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-docker run -t -d --name kali2 -e vnc_passwd="$0" -p 25901:5900 -p 25905:5901 boomer
-=======
-docker run -t -d --name kali2 -e vnc_passwd=$1 -p 25901:5900 -p 25905:5901 kali-13-10
->>>>>>> 133ad63 (Deleted varaible.sh script, and some small changes to the run container script)
+docker run -t -d --name kali2 -e vnc_passwd=$1 -p 25901:5900 -p 25905:5901 $2
