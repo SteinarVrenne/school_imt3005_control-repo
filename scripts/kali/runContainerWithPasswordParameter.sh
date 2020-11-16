6deae9fac915b9ccb7a70ddfeb6781691ef2370c
Harald <haraar@stud.ntnu.no>
2020-11-16 17:56:11 +0100

    Added a seperate dockerfile for each of the kali variants

--- scripts/kali/runContainerWithPasswordParameter.sh
+++ scripts/kali/runContainerWithPasswordParameter.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 docker run -t -d --name kali2 -e vnc_passwd="$0" -p 25901:5900 -p 25905:5901 boomer
+=======
+docker run -t -d --name kali2 -e vnc_passwd=$1 -p 25901:5900 -p 25905:5901 kali-13-10
+>>>>>>> 133ad63 (Deleted varaible.sh script, and some small changes to the run container script)
