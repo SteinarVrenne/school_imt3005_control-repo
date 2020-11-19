54bf634d957f971446dde20b1d38764db45a9b8e
jimbob <abdifatah87@live.no>
2020-11-19 05:48:25 +0100

    Added code for starting up new machine from manager

--- scripts/kali/managerscript.py
+++ scripts/kali/managerscript.py
@@ -5,17 +5,13 @@ import subproces, time, sys
 # Get srv IP
 allIp = str(subprocess.check_output('./getips.sh')).lstrip("b'").rstrip("\\n'").split(",")
 
-# Get amount of containers in srv1
-#containers = []
-#for i in allIp:
-#    containers.append(subprocess.getoutput('ssh root@' +str(i)' docker ps -a | wc -l'))
-
 # Number of the first ports used to run a docker container.
 
 # In order to run a container. It has to increment the port value by 1, and take in the kali variant parameter
 # runcontainer = subprocess.call('docker run -t -d --name kali4 -p' + str(port1++) +':5900 -p' + str(port2++) +':5901' kali #Kali should be "Pentest, Forensics or Stego"
 
-# getports = str(subprocess.call("docker ps | awk '{print $10, $11}'", shell=True)).split("tcp")
+# Print ut ip adressen til den nye maskinen som er laget, slik at web serveren kan hente dette via standard in/out
+# echo laget dockercontainers ip:port
 
 def send_ip(vnc, flavor):
 port1 = 25901
@@ -35,7 +31,7 @@ newMachine = True
                 break
     if newMachine == True:
         # Start a new machine
-        newmachine = subprocess.run('blablabla start srv3...')
+        subprocess.call('../newHostMachine.sh')
         sleep(10)
         allIp = str(subprocess.check_output('./getips.sh')).lstrip("b'").rstrip("\\n'").split(",")
         subprocess.call("docker run -t -d --name kali"+str(num)+" -e vnc_passwd="+vnc+" -e flavour="+flavor+" -p "+str(port1)+":5900 -p "+str(port2)+":5901 kali", shell=True)
@@ -46,26 +42,7 @@ newMachine = True
         subprocess.call("docker run -t -d --name kali"+str(num)+" -e vnc_passwd="+vnc+" -e flavour="+flavor+" -p "+str(port1)+":5900 -p "+str(port2)+":5901 kali", shell=True)
         print(ipadd+":"+str(port2))
 
-
-
-# Check if the server has the correct docker images, run this script
-# buildContainers.sh
-
-
-# If srv1 has more or less than 10 containers
-if containers < 10:
-    # If number is less than 10, then run a new container from the variable runcontainer.
-    subprocess.call("docker run -t -d --name kali1 -p 25902:5900 -p 25912:5901 kali", shell=True)
-   # Pseudokode for implementering av variabler
-   subprocess.call("docker run -t -d --name kali$number -e vnc_passwd=$arg1 -e flavour=$arg2 -p $port1:5900 -p $port2:5901 kali", shell=True)
-
-else:
-    # Else chech how many containers there are no server 2. If server 2 is not present, create it
-     containers = subprocess.getoutput('ssh ubuntu@' +str(server2ip)' -i gruppe4.pem docker ps -a | wc -l')
-
-
 if __name__ == __main__:
     send_ip(str(sys.argv[1]), str(sys.argv[2]))
 
-# Print ut ip adressen til den nye maskinen som er laget, slik at web serveren kan hente dette via standard in/out
-# echo laget dockercontainers ip:port
+
--- scripts/newHostMachine.sh
+++ scripts/newHostMachine.sh
@@ -5,6 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 # Source the config file
 source ./iac_variables.conf
 
+source /root/openRC.sh
+
 command='openstack server create --image "'$image'" --flavor "'$flavor'" --security-group "'$linuxSecGroup'" --security-group "'$iacSecGroup'" --key-name "'$keyname'" --user-data "/etc/puppetlabs/code/environment/production/scripts/host_machine_boot.sh" '$1''
 
 # echo $command
