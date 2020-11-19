479a33af09cf10554820c71a5a368f399fcae112
jimbob <abdifatah87@live.no>
2020-11-19 06:32:54 +0100

    fixed errors in the code

--- scripts/kali/managerscript.py
+++ scripts/kali/managerscript.py
@@ -2,9 +2,6 @@
 
 import subprocess, time, sys
 
-# Get srv IP
-allIp = str(subprocess.check_output('./script/getips.sh')).lstrip("b'").rstrip("\\n'").split(",")
-
 # Number of the first ports used to run a docker container.
 
 # In order to run a container. It has to increment the port value by 1, and take in the kali variant parameter
@@ -14,13 +11,14 @@ allIp = str(subprocess.check_output('./script/getips.sh')).lstrip("b'").rstrip("
 # echo laget dockercontainers ip:port
 
 def send_ip(vnc, flavor):
+    # Get srv IP
+    allIp = str(subprocess.check_output('./script/getsrvip.sh')).lstrip("b'").rstrip("\\n'").split(",")
     port1 = 25901
     port2 = 25911
-    ipadd = ""
     num = 1
     newMachine = True
     for i in allIp:
-        ipadd = allIp[i]
+        ipadd = str(i)
         for j in range(1, 11):
             port1 = int(str(subprocess.check_output("ssh root@"+ipadd+" docker port kali"+str(j)+" 5900", shell=True)).strip("0.0.0.0:"))
             port2 = int(str(subprocess.check_output("ssh root@"+ipadd+" docker port kali"+str(j)+" 5901", shell=True)).strip("0.0.0.0:"))
@@ -42,7 +40,8 @@ def send_ip(vnc, flavor):
         subprocess.call("docker run -t -d --name kali"+str(num)+" -e vnc_passwd="+vnc+" -p "+str(port1)+":5900 -p "+str(port2)+":5901 "+flavor, shell=True)
         print(ipadd+":"+str(port2))
 
-if __name__ == __main__:
+if __name__ == "__main__":
+    print(str(sys.argv[1]), str(sys.argv[2]))
     send_ip(str(sys.argv[1]), str(sys.argv[2]))
 
 
