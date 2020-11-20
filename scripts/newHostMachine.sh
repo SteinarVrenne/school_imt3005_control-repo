94946523932c95bbfd952e88de9cda30e2422301
jimbob <abdifatah87@live.no>
2020-11-20 09:10:55 +0100

    added missing parentheses

--- scripts/newHostMachine.sh
+++ scripts/newHostMachine.sh
@@ -7,7 +7,7 @@ source /etc/puppetlabs/code/environments/production/scripts/iac_variables.conf
 
 source /root/openRC.sh
 
-command='openstack server create --image "'$image'" --flavor "'$flavor'" --security-group "'$linuxSecGroup'" --security-group "'$iacSecGroup'" --key-name "'$keyname'" --user-data "/etc/puppetlabs/code/environment/production/scripts/host_machine_boot.sh" '$1''
+command='openstack server create --image "'$image'" --flavor "'$flavor'" --security-group "'$linuxSecGroup'" --security-group "'$iacSecGroup'" --key-name "'$keyname'" --user-data /etc/puppetlabs/code/environments/production/scripts/host_machine_boot.sh '$1''
 
 # echo $command
 
