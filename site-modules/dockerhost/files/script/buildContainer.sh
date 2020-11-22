9ef2d1276b46624baf88bfaad8e5cf51b05ccc87
Viramos <martiwah@stud.ntnu.no>
2020-11-22 16:28:46 +0100

    .. instead of . in docker images paths

--- site-modules/dockerhost/files/script/buildContainer.sh
+++ site-modules/dockerhost/files/script/buildContainer.sh
@@ -3,11 +3,11 @@ docker pull haraldaarz/kali_xfce_vnc_top10
 docker build -t kali_base -f ../images/kali_base/Dockerfile .
 
 # Builds the pentest image
-docker build -t pentest ./images/pentest
+docker build -t pentest ../images/pentest
 
 # Builds the forensics image
-docker build -t forensics ./images/forensics
+docker build -t forensics ../images/forensics
 
 
 # Builds the stego image
-docker build -t stego ./images/stego
+docker build -t stego ../images/stego
