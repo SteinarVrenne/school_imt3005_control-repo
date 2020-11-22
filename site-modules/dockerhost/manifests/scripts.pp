c5e0062f02175100480f0f4a6c2fde2cebccda3a
Viramos <martiwah@stud.ntnu.no>
2020-11-22 15:08:24 +0100

    Moving files to where they need to be via puppet

--- site-modules/dockerhost/manifests/scripts.pp
+++ site-modules/dockerhost/manifests/scripts.pp
@@ -2,15 +2,25 @@ class dockerhost::scripts {
 
     require dockerhost::images
     # Ensure buildContainer.sh is on the servers and run it once
-    file { '/root/buildContainer.sh':
-        source => "puppet:///modules/dockerhost/buildContainer.sh",
-        mode => "0755",
+    file { '/root/script/buildContainer.sh':
+        source => "puppet:///modules/dockerhost/script/buildContainer.sh",
+        mode   => "0755",
     }
 
-    exec { 'bash /root/buildContainer.sh':
+    exec { 'bash /root/script/buildContainer.sh':
         path => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
         user => 'root',
         cwd => '/root/',
-        subscribe => [ File['/root/images/kali_base/Dockerfile'], File['/root/buildContainer.sh'] ]
+        subscribe => [ File['/root/images/kali_base/Dockerfile'], File['/root/script/buildContainer.sh'], File['/root/script/vncpasswd.sh'], File['/root/script/vnc.sh'] ]
+    }
+
+    file { '/root/script/vncpasswd.sh':
+        source => "puppet:///modules/dockerhost/script/vncpasswd.sh",
+        mode => "0755",
+    }
+
+    file { '/root/script/vnc.sh':
+        source => "puppet:///modules/dockerhost/script/vnc.sh",
+        mode => "0755",
     }
 }
