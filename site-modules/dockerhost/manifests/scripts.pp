ca82aeb17d0047823a290cda1a40950d35992108
Viramos <martiwah@stud.ntnu.no>
2020-11-22 15:15:28 +0100

    Added directory script

--- site-modules/dockerhost/manifests/scripts.pp
+++ site-modules/dockerhost/manifests/scripts.pp
@@ -1,26 +1,34 @@
 class dockerhost::scripts {
 
     require dockerhost::images
+
+    file { '/root/script':
+        ensure => 'directory',
+        mode   => "0755",
+    }
     # Ensure buildContainer.sh is on the servers and run it once
     file { '/root/script/buildContainer.sh':
-        source => "puppet:///modules/dockerhost/script/buildContainer.sh",
-        mode   => "0755",
+        source  => "puppet:///modules/dockerhost/script/buildContainer.sh",
+        mode    => "0755",
+        require => File['/root/script'],
     }
 
     exec { 'bash /root/script/buildContainer.sh':
         path => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin',
         user => 'root',
         cwd => '/root/',
-        subscribe => [ File['/root/images/kali_base/Dockerfile'], File['/root/script/buildContainer.sh'], File['/root/script/vncpasswd.sh'], File['/root/script/vnc.sh'] ]
+        subscribe => [ File['/root/images/kali_base/Dockerfile'], File['/root/script/buildContainer.sh'], File['/root/script/vncpasswd.sh'], File['/root/script/vnc.sh'] ],
     }
 
     file { '/root/script/vncpasswd.sh':
         source => "puppet:///modules/dockerhost/script/vncpasswd.sh",
         mode => "0755",
+        require => File['/root/script'],
     }
 
     file { '/root/script/vnc.sh':
         source => "puppet:///modules/dockerhost/script/vnc.sh",
         mode => "0755",
+        require => File['/root/script'],
     }
 }
