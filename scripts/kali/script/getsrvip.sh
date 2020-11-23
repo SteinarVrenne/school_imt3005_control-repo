257531bc00c50845c64b73bf482b1dff1888c08e
Viramos <martiwah@stud.ntnu.no>
2020-11-23 15:09:26 +0100

    altered to look for OpenRC in the right spot

--- scripts/kali/script/getsrvip.sh
+++ scripts/kali/script/getsrvip.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source /home/ubuntu/openRC.sh
+source /root/openRC.sh
 var=$(openstack server list | grep srv | awk '{print $9}')
 prefix="iac_admin_net="
 suffix=","
-for i in $var 
+for i in $var
 do
 foo+=${i#"$prefix"}
 foo+=','
